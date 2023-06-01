35be62eeffa80ca6fb15512803c5d0c5955f2fb9
GianlucaFuwa <51165364+GianlucaFuwa@users.noreply.github.com>
2023-06-01 17:35:19 +0200

    Update metropolis.jl

--- src/updates/metropolis.jl
+++ src/updates/metropolis.jl
@@ -1,22 +1,26 @@
-struct MetroUpdate <: AbstractUpdate
+struct MetroUpdate{TG} <: AbstractUpdate
 	ϵ::Base.RefValue{Float64}
 	multi_hit::Int64
 	target_acc::Float64
 	mnorm::Float64
 	meta_enabled::Bool
-	_temp_for_staples::TemporaryField
 
-	function MetroUpdate(U::Gaugefield, ϵ, multi_hit, target_acc, meta_enabled)
+	function MetroUpdate(
+		U::Gaugefield{TG},
+		ϵ,
+		multi_hit,
+		target_acc,
+		meta_enabled,
+	) where {TG}
 		mnorm = 1 / (U.NV * 4 * multi_hit)
 		m_ϵ = Base.RefValue{Float64}(ϵ)
-		_temp_for_staples = TemporaryField(U)
-		return new(m_ϵ, multi_hit, target_acc, mnorm, meta_enabled, _temp_for_staples)
+		return new{TG}(m_ϵ, multi_hit, target_acc, mnorm, meta_enabled)
 	end
 end
 
 function update!(
-	updatemethod::MetroUpdate,
-	U::Gaugefield,
+	updatemethod,
+	U,
 	verbose::VerboseLevel;
 	Bias = nothing,
 	metro_test = true,
@@ -32,29 +36,29 @@ function update!(
 end
 
 function metro_sweep!(
-	U::Gaugefield,
-	metro::MetroUpdate,
+	U,
+	metro::MetroUpdate{TG};
 	metro_test = true,
-)
+) where {TG}
 	NX, NY, NZ, NT = size(U)
 	β = U.β
 	ϵ = metro.ϵ[]
 	multi_hit = metro.multi_hit
 	numaccept = 0
 
-	A = metro._temp_for_staples
-	staple_eachsite!(A, U)
+	staple = TG()
 
 	for it in 1:NT
 		for iz in 1:NZ
 			for iy in 1:NY
 				for ix in 1:NX
+					site = SiteCoords(ix, iy, iz, it)
 					for μ in 1:4
 
-						for i in 1:multi_hit
+						for _ in 1:multi_hit
 							X = gen_SU3_matrix(ϵ)
 							link = U[μ][ix,iy,iz,it]
-							A_adj = A[μ][ix,iy,iz,it]'
+							A_adj = staple(U, μ, site)'
 							ΔSg = β/3 * real(tr((X * link - link) * A_adj))
 
 							accept = metro_test ? (rand() ≤ exp(-ΔSg)) : 1
@@ -77,30 +81,30 @@ function metro_sweep!(
 end
 
 function metro_sweep_meta!(
-	U::Gaugefield,
-	Bias::BiasPotential,
-	metro::MetroUpdate,
+	U,
+	Bias,
+	metro::MetroUpdate{TG};
 	metro_test = true,
-)
+) where {TG}
 	NX, NY, NZ, NT = size(U)
 	β = U.β
 	ϵ = metro.ϵ[]
 	multi_hit = metro.multi_hit
 	numaccept = 0
 
-	A = metro._temp_for_staples
-	staple_eachsite!(A, U)
+	staple = TG()
 
 	for it in 1:NT
 		for iz in 1:NZ
 			for iy in 1:NY
 				for ix in 1:NX
+					site = SiteCoords(ix, iy, iz, it)
 					for μ in 1:4
-						
-						for hit in 1:multi_hit
+
+						for _ in 1:multi_hit
 							X = gen_SU3_matrix(ϵ)
 							link = U[μ][ix,iy,iz,it]
-							A_adj = A[μ][ix,iy,iz,it]'
+							A_adj = staple(U, μ, site)'
 
 							CV = U.CV
 							ΔSg = β/3 * real(tr((X * link - link) * A_adj))
@@ -127,7 +131,7 @@ function metro_sweep_meta!(
 	return numaccepts
 end
 
-function adjust_ϵ!(metro::MetroUpdate, numaccepts)
+function adjust_ϵ!(metro, numaccepts)
 	metro.ϵ[] += (numaccepts * metro.mnorm - metro.target_acc) * 0.2
 	return nothing
 end
