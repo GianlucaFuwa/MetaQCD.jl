725f6c8d4a644f167dde8d875d8feacf2a152776
GianlucaFuwa <gianlucafuwa@gmail.com>
2023-06-19 01:09:39 +0200

    update mainrun.jl

--- src/system/mainrun.jl
+++ src/system/mainrun.jl
@@ -4,14 +4,13 @@ module Mainrun
     using InteractiveUtils
     using Random
     using ..VerbosePrint
-    
+
     import ..AbstractMeasurementModule: measure
     import ..AbstractSmearingModule: GradientFlow, Euler, RK2, RK3, RK3W7
     import ..AbstractUpdateModule: HeatbathUpdate, HMCUpdate, MetroUpdate, Updatemethod
-    import ..AbstractUpdateModule: update!
+    import ..AbstractUpdateModule: AbstractUpdate, update!
     import ..Gaugefields: AbstractGaugeAction, DBW2GaugeAction, IwasakiGaugeAction,
         SymanzikTreeGaugeAction, SymanzikTadGaugeAction, WilsonGaugeAction
-    import ..Gaugefields: normalize!
     import ..Metadynamics: MetaDisabled, MetaEnabled
     import ..MetaQCD: MeasurementMethods, calc_measurements, calc_measurements_flowed
     import ..ParametersTOML: construct_params_from_toml
@@ -23,18 +22,19 @@ module Mainrun
         filename_head = splitext(filenamein)[1]
         filename = filename_head * ".toml"
         parameters = construct_params_from_toml(filename)
-        
+
         Random.seed!(parameters.randomseed)
-        
+
         univ = Univ(parameters)
         println_verbose1(univ.verbose_print, "# ", pwd())
-
         println_verbose1(univ.verbose_print, "# ", Dates.now())
         io = IOBuffer()
 
         InteractiveUtils.versioninfo(io)
         versioninfo = String(take!(io))
         println_verbose1(univ.verbose_print, versioninfo)
+
+        println("\t>> Universe is set!\n")
 
         run_sim!(univ, parameters)
 
@@ -44,14 +44,21 @@ module Mainrun
     function run_sim!(univ, parameters)
         U = univ.U
 
-        tmp = Updatemethod(parameters, U[1], instance = 1)
-        updatemethod = Vector{typeof(tmp)}(undef, parameters.numinstances)
-        updatemethod[1] = tmp
+        updatemethod = Vector{AbstractUpdate}(undef, parameters.numinstances)
+        updatemethod[1] = Updatemethod(parameters, U[1])
 
 
         for i in 2:parameters.numinstances
-            updatemethod[i] = Updatemethod(parameters, U[i], instance = i)
+            updatemethod[i] = HMCUpdate(
+                U[i],
+                parameters.hmc_integrator,
+                parameters.hmc_steps,
+                parameters.hmc_Δτ,
+                meta_enabled = true,
+            )
         end
+
+        println("\t>> Updatemethods are set!\n")
 
         gradient_flow = GradientFlow(
             U[1],
@@ -73,6 +80,8 @@ module Mainrun
             parameters.measurements_with_flow,
             flow = true,
         )
+
+        println("\t>> Measurement methods are set!\n")
 
         # savedata = Savedata(
         #     parameters.saveU_format,
@@ -102,17 +111,16 @@ module Mainrun
         measurements,
         measurements_with_flow,
     )
+        numinstances = parameters.numinstances
+
         U = univ.U
         Bias = univ.Bias
 
-        numinstances = parameters.numinstances
-
         if parameters.meta_enabled
-            meta_charge_fp = Vector{IOStream}(undef, numinstances)
+            meta_charge_fp = Vector{Verbose1}(undef, numinstances)
             for i in 1:numinstances
-                meta_charge_fp[i] = open(
-                    parameters.measuredir * "/Meta_charge_$i.txt",
-                    "w",
+                meta_charge_fp[i] = Verbose1(
+                    parameters.measuredir * "/meta_charge_$i.txt",
                 )
             end
         else
@@ -135,54 +143,64 @@ module Mainrun
                         metro_test = false,
                     )
                     updatetime += runtime
-                    normalize!(U[i])
                 end
 
                 println_verbose1(
                     univ.verbose_print,
-                    "Thermalization Update: Elapsed time $updatetime [s]"
+                    ">> Thermalization Update: Elapsed time $(sum(updatetime)) [s]\n"
                 )
             end
         end
 
         println_verbose1(
             univ.verbose_print,
-            "Thermalization Elapsed time $(runtime_therm) [s]"
+            "\t>> Thermalization Elapsed time $(runtime_therm) [s]\n"
         )
 
         value, runtime_all = @timed begin
-            numaccepts = zeros(AbstractFloat, numinstances)
+            numaccepts = zeros(numinstances)
+            numaccepts_temper = numinstances > 1 ? zeros(Int64, numinstances - 1) : nothing
 
             for itrj in 1:parameters.Nsteps
-                println_verbose1(univ.verbose_print, "# itrj = $itrj")
-                updatetime = 0.0
-                
-                for i in 1:numinstances
-                    accepted, runtime = @timed update!(
-                        updatemethod[i],
-                        U[i],
-                        univ.verbose_print,
-                        Bias = Bias[i],
-                    )
-                    numaccepts[i] += accepted
-                    updatetime += runtime
-                    normalize!(U[i])
+                println_verbose1(univ.verbose_print, "\n# itrj = $itrj")
+
+                _, updatetime = @timed begin
+                    for i in 1:numinstances
+                        accepted = update!(
+                            updatemethod[i],
+                            U[i],
+                            univ.verbose_print,
+                            Bias = Bias[i],
+                            metro_test = true,
+                        )
+                        numaccepts[i] += accepted
+                    end
                 end
 
                 println_verbose1(
                     univ.verbose_print,
-                    "Update: Elapsed time $updatetime [s]"
+                    ">> Update: Elapsed time $(sum(updatetime)) [s]"
                 )
-                
+
                 if parameters.tempering_enabled
-                    for i in numinstances:-1:2
-                        accepted = temper!(
-                            U[i],
-                            U[i-1],
-                            Bias[i],
-                            Bias[i-1],
-                        )
-                        numaccepts_temper[i-1] += ifelse(accepted, 1, 0)
+                    swap_every = parameters.swap_every
+                    if itrj % swap_every == 0
+                        for i in numinstances:-1:2
+                            accepted =  temper!(
+                                U[i],
+                                U[i-1],
+                                Bias[i],
+                                Bias[i-1],
+                                univ.verbose_print
+                            )
+                            numaccepts_temper[i-1] += accepted
+
+                            println_verbose1(
+                                univ.verbose_print,
+                                ">> Swap Acceptance [$i ⇔  $(i-1)] $itrj : ",
+                                "$(numaccepts_temper[i-1] * 100 / (itrj / swap_every)) %"
+                            )
+                        end
                     end
                 end
 
@@ -203,35 +221,40 @@ module Mainrun
 
                 if parameters.meta_enabled
                     for i in 1:numinstances
-                        println(
+                        println_verbose1(
                             meta_charge_fp[i],
-                            "$itrj $(U[i].CV) # metacharge",
+                            "$itrj $(U[i].CV) # metacharge_$i",
                         )
                         flush(meta_charge_fp[i])
                     end
                 end
 
-                println_verbose1(
-                    univ.verbose_print,
-                    "Acceptance $itrj : $(numaccepts[1]*100/itrj) %"
-                )
+                for i in 1:numinstances
+                    println_verbose1(
+                        univ.verbose_print,
+                        ">> Acceptance $i $itrj : ",
+                        numaccepts[i] * 100 / itrj,
+                        "%",
+                    )
+                end
+
                 flush(univ.verbose_print.fp)
             end
         end
 
         if parameters.meta_enabled
-
             for i in 1:numinstances
                 writedlm(
                     Bias[i].fp,
                     [Bias[i].bin_vals Bias[i].values],
                 )
 
-                close(univ.Bias[i].fp)
+                close(Bias[i].fp)
+                close(meta_charge_fp[i].fp)
 
                 println_verbose1(
                     univ.verbose_print,
-                    "Metapotential $i has been saved in file \"$(Bias[i].fp)\""
+                    "\t>> Metapotential $i has been saved in file \"$(Bias[i].fp)\""
                 )
                 #=
                 q_vals = readdlm(
@@ -257,7 +280,7 @@ module Mainrun
         flush(stdout)
         flush(univ.verbose_print)
 
-        println_verbose1(univ.verbose_print, "Total Elapsed time $(runtime_all) [s]")
+        println_verbose1(univ.verbose_print, "\t>> Total Elapsed time $(runtime_all) [s]\n")
 
         return nothing
     end
@@ -284,10 +307,10 @@ module Mainrun
             return new(issaved, saveU_format, saveU_dir, saveU_every, itrjsavecount)
         end
     end
-    #= 
+    #=
     function save_gaugefield(savedata::Savedata, U, itrj)
         if savedata.issaved == false
-            return 
+            return
         end
 
         if itrj % savedata.itrjsavecount == 0
