aac7e0b008160184009dfdacf9bbbfe72104ca88
GianlucaFuwa <gianlucafuwa@gmail.com>
2023-06-21 17:29:54 +0200

    add build.jl

---
+++ build.jl
@@ -0,0 +1,16 @@
+include("./src/MetaQCD.jl")
+using .MetaQCD
+
+if length(ARGS) == 0
+    error("""
+    Use input file, like:
+    julia run.jl parameters.toml
+    """
+    )
+end
+
+function build()
+    run_build(ARGS[1])
+end
+
+@time build()
