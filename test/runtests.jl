c51695f7de9cb981619b9cd631fce6fa861e3d90
GianlucaFuwa <gianlucafuwa@gmail.com>
2024-08-12 02:07:56 +0200

    Comment out CUDA tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,7 @@ using Aqua
 using Random
 using Test
 using Polyester
-using CUDA
+# using CUDA
 
 include("./test_io.jl")
 include("./test_checkpoint.jl")
@@ -21,7 +21,7 @@ include("./test_clinalg.jl")
     test_cdot()
     test_ckron()
     test_cmvmul()
-    # test_spin_color()
+    # test_spin_color() # FIXME: Fix these tests
     test_cmatmul()
 end
 
@@ -66,44 +66,43 @@ end
     @test test_update(backend; update_method="hmc")
 end
 
-if CUDA.functional(true)
-# if false
-    @testset "CUDA Tests" begin
-        CUDA.allowscalar(false)
-        backend = CUDABackend
-        plaq, poly, tc_plaq, tc_clover, tc_improved, wl_1x1 = test_measurements(backend)
-        @test isapprox(0.587818337847024, plaq)
-        @test isapprox(0.5255246068616176 - 0.15140850971249734im, poly)
-        @test isapprox(-0.2730960126400261, tc_plaq)
-        @test isapprox(-0.027164585971545994, tc_clover)
-        @test isapprox(-0.03210085960569041, tc_improved)
-        @test isapprox(0.587818337847024, wl_1x1)
-
-        relerrors = CUDA.@allowscalar(test_derivative(backend))
-        @test length(findall(x -> abs(x) > 1e-4, relerrors[:, 2])) == 0
-        relerrors = CUDA.@allowscalar(test_fderivative(
-            backend; dirac="staggered", mass=0.01, single_flavor=true
-        ))
-        @test length(findall(x -> abs(x) > 1e-4, relerrors[:, 2])) == 0
-        # relerrors = CUDA.@allowscalar(test_fderivative(
-        #     backend; dirac="staggered", mass=0.01, single_flavor=true, eoprec=true
-        # ))
-        # @test length(findall(x -> abs(x) > 1e-4, relerrors[:, 2])) == 0
-        relerrors = CUDA.@allowscalar(test_fderivative(
-            backend; dirac="wilson", mass=0.01, single_flavor=true
-        ))
-        @test length(findall(x -> abs(x) > 1e-4, relerrors[:, 2])) == 0
-        # relerrors = CUDA.@allowscalar(test_fderivative(
-        #     backend; dirac="wilson", mass=0.01, single_flavor=false, eoprec=true
-        # ))
-        # @test length(findall(x -> abs(x) > 1e-4, relerrors[:, 2])) == 0
-
-        @test test_gradflow(backend)
-        @test test_update(backend; update_method="heatbath")
-        @test test_update(backend; update_method="metropolis", gaction=IwasakiGaugeAction)
-        @test test_update(backend; update_method="hmc")
-    end
-end
+# if CUDA.functional(true)
+#     @testset "CUDA Tests" begin
+#         CUDA.allowscalar(false)
+#         backend = CUDABackend
+#         plaq, poly, tc_plaq, tc_clover, tc_improved, wl_1x1 = test_measurements(backend)
+#         @test isapprox(0.587818337847024, plaq)
+#         @test isapprox(0.5255246068616176 - 0.15140850971249734im, poly)
+#         @test isapprox(-0.2730960126400261, tc_plaq)
+#         @test isapprox(-0.027164585971545994, tc_clover)
+#         @test isapprox(-0.03210085960569041, tc_improved)
+#         @test isapprox(0.587818337847024, wl_1x1)
+#
+#         relerrors = CUDA.@allowscalar(test_derivative(backend))
+#         @test length(findall(x -> abs(x) > 1e-4, relerrors[:, 2])) == 0
+#         relerrors = CUDA.@allowscalar(test_fderivative(
+#             backend; dirac="staggered", mass=0.01, single_flavor=true
+#         ))
+#         @test length(findall(x -> abs(x) > 1e-4, relerrors[:, 2])) == 0
+#         # relerrors = CUDA.@allowscalar(test_fderivative(
+#         #     backend; dirac="staggered", mass=0.01, single_flavor=true, eoprec=true
+#         # ))
+#         # @test length(findall(x -> abs(x) > 1e-4, relerrors[:, 2])) == 0
+#         relerrors = CUDA.@allowscalar(test_fderivative(
+#             backend; dirac="wilson", mass=0.01, single_flavor=true
+#         ))
+#         @test length(findall(x -> abs(x) > 1e-4, relerrors[:, 2])) == 0
+#         # relerrors = CUDA.@allowscalar(test_fderivative(
+#         #     backend; dirac="wilson", mass=0.01, single_flavor=false, eoprec=true
+#         # ))
+#         # @test length(findall(x -> abs(x) > 1e-4, relerrors[:, 2])) == 0
+#
+#         @test test_gradflow(backend)
+#         @test test_update(backend; update_method="heatbath")
+#         @test test_update(backend; update_method="metropolis", gaction=IwasakiGaugeAction)
+#         @test test_update(backend; update_method="hmc")
+#     end
+# end
 
 if VERSION >= v"1.9"
     Aqua.test_all(MetaQCD; stale_deps=false, ambiguities=false)
