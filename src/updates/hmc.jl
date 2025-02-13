d0d7220d01c046e35b9b6745d92ced8bee0e155e
GianlucaFuwa <gianlucafuwa@gmail.com>
2025-02-13 14:20:56 +0100

    Print whether hmc update was accepted in hmc-logfile [skip ci]

--- src/updates/hmc.jl
+++ src/updates/hmc.jl
@@ -162,7 +162,7 @@ function HMC(
                 @printf(
                     fp,
                     "%-25s%-25s%-25s%-25s%-25s\n",
-                    "ΔP²", "ΔSg", "ΔSf", "ΔV", "ΔH",
+                    "ΔP²", "ΔSg", "ΔSf", "ΔV", "ΔH", "Accepted"
                 )
             end
         end
@@ -266,7 +266,7 @@ function update!(
 
     ΔH = ΔP² + ΔSg + ΔV + ΔSf
     accept = metro_test ? rand() ≤ exp(-ΔH) : true
-    print_hmc_data(hmc.logfile, ΔP², ΔSg, ΔSf, ΔV, ΔH)
+    print_hmc_data(hmc.logfile, ΔP², ΔSg, ΔSf, ΔV, ΔH, accept)
 
     if accept
         U.Sg = Sg_new
@@ -437,22 +437,24 @@ function calc_fermion_action(fermion_action, U, ϕ, smearing::StoutSmearing, is_
     return Sf
 end
 
-@inline function print_hmc_data(::Nothing, ΔP², ΔSg, ΔSf, ΔV, ΔH)
+@inline function print_hmc_data(::Nothing, ΔP², ΔSg, ΔSf, ΔV, ΔH, accept)
     @level2("delta_P²:\t$ΔP²")
     @level2("delta_Sg:\t$ΔSg")
     @level2("delta_Sf:\t$ΔSf")
     @level2("delta_V:\t$ΔV")
     @level2("delta_H:\t$ΔH")
+    @level2("Accepted:\t$(Int64(accept))")
     return nothing
 end
 
-@inline function print_hmc_data(logfile, ΔP², ΔSg, ΔSf, ΔV, ΔH)
+@inline function print_hmc_data(logfile, ΔP², ΔSg, ΔSf, ΔV, ΔH, accept)
     fp = fopen(logfile, "a")
     printf(fp, "%+-25.15E", ΔP²)
     printf(fp, "%+-25.15E", ΔSg)
     printf(fp, "%+-25.15E", ΔSf)
     printf(fp, "%+-25.15E", ΔV)
     printf(fp, "%+-25.15E", ΔH)
+    printf(fp, "%-25.1i", Int64(accept))
     newline(fp)
     fclose(fp)
     return nothing
